4c2a1d6b4a5cae1ffaf27cca7279f34b0de74124
Yonathan Misgan Mengie <yonathanm.dev@gmail.com>
2026-06-29 18:53:04 +0300

    Add by force yonathan

--- views/sub-sector-registration.php
+++ views/sub-sector-registration.php
@@ -137,6 +137,7 @@ $is_sub_sector_registration_page = true;
                   data-restrict="letters"
                   required
                 >
+                
                 <div class="invalid-feedback">
                   እባክዎ የንዑስ ዘርፍ ስም ያስገቡ (ቢያንስ 2 ፊደላት)
                 </div>
