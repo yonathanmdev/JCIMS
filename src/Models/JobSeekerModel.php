f46f2eef586fc4553cdab9bc24d78a459b735a2d
Yonathan Misgan Mengie <yonathanm.dev@gmail.com>
2026-06-30 17:29:29 +0300

    job seeker registration model

--- src/Models/JobSeekerModel.php
+++ src/Models/JobSeekerModel.php
@@ -11,48 +11,99 @@ class JobSeekerModel {
     }
 
 
- public function createJobseeker(array $data): bool {
-        try {
-            $fullNameRaw = $data['first_name'] . ' ' . $data['father_name'] . ' ' . $data['g_father_name'];
-            $normalizedFullName = AmharicNormalizer::normalize($fullNameRaw);
-            // Start transaction
-            $this->db->beginTransaction();
-          // Insert employee
-            $sql = "INSERT INTO job_seekers (
-                id, first_name, father_name, last_name, gender, organization_id,
-                branch_id, full_name_normalized, registered_by
-            ) VALUES (
-                ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+public function createJobseeker(array $data): bool {
+    try {
+        $fullNameRaw = $data['first_name'] . ' ' . $data['father_name'] . ' ' . $data['g_father_name'];
+        $normalizedFullName = AmharicNormalizer::normalize($fullNameRaw);
+        // Start transaction
+        $this->db->beginTransaction();
+        // Insert employee
+        $sql = "INSERT INTO job_seekers (id, branch_id, first_name, father_name, last_name, gender, 
+        age, educational_level, educated_dpt, education_trmnet_finsh_year, g8id, CGPA, school_type, 
+        physical_condition, physical_condition_desc, kebele, mender, kebele_id_no, phone_number, 
+        choice_sector1, sub_choose1, choice_sector2, sub_choose2, choice_sector3, sub_choose3, 
+        meteleya_huneta, residence_status, srafelagi_huneta, graguation_catagory, maritalstatus, 
+        noofmonth, haveexp, workplace, experience, profession, nameofcountry, language,
+         wageorself, typeofemployment, regstration_level, orgstatus, mothername, Labor_ID, 
+         FAN, kebele_id_photo, jsphoto, fiscal_year,
+         agri_business_experience_status, agri_business_experience, has_dependents, 
+         number_of_dependents, children_under_five, full_name_normalized, registered_by) VALUES (
+            ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 
+            ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-            $stmt = $this->db->prepare($sql);
-            $result1 = $stmt->execute([
-                $data['uuid'],
-                $data['first_name'],
-                $data['father_name'],
-                $data['g_father_name'],
-                $data['sex'],
-                $data['organization_id'],
-                $data['branch_id'],
-                $normalizedFullName,
-                $data['reg_by']
-            ]);
+        $stmt = $this->db->prepare($sql);
+        $result1 = $stmt->execute([
+            $data['uuid'],
+            $data['branch_id'],
+            $data['first_name'],
+            $data['father_name'],
+            $data['last_name'],
+            $data['gender'],
+            $data['age'],
+            $data['educational_level'],
+            $data['educated_dpt'],
+            $data['education_trmnet_finsh_year'],
+            $data['g8id'],
+            $data['CGPA'],
+            $data['school_type'],
+            $data['physical_condition'],
+            $data['physical_condition_desc'],
+            $data['kebele'],
+            $data['mender'],
+            $data['kebele_id_no'],
+            $data['phone_number'],
+            $data['choice_sector1'],
+            $data['sub_choose1'],
+            $data['choice_sector2'],
+            $data['sub_choose2'],
+            $data['choice_sector3'],
+            $data['sub_choose3'],
+            $data['meteleya_huneta'],
+            $data['residence_status'],
+            $data['srafelagi_huneta'],
+            $data['graguation_catagory'],
+            $data['maritalstatus'],
+            $data['noofmonth'],
+            $data['haveexp'],
+            $data['workplace'],
+            $data['experience'],
+            $data['profession'],
+            $data['nameofcountry'],
+            $data['language'],
+            $data['wageorself'],
+            $data['typeofemployment'],
+            $data['regstration_level'],
+            $data['orgstatus'],
+            $data['mothername'],
+            $data['Labor_ID'],
+            $data['FAN'],
+            $data['kebele_id_photo'],
+            $data['jsphoto'],
+            $data['fiscal_year'],
+            $data['agri_business_experience_status'],
+            $data['agri_business_experience'],
+            $data['has_dependents'],
+            $data['number_of_dependents'],
+            $data['children_under_five'],
+            $normalizedFullName,
+            $data['reg_by']
+        ]);
 
-            if (!$result1) {
-                throw new \Exception("Failed to insert employee");
-            }
-
-
-            // Commit transaction
-            $this->db->commit();
-            return true;
-
-        } catch (\Exception $e) {
-            // Rollback transaction on error
-            $this->db->rollBack();
-            error_log("Job seeker registration transaction failed: " . $e->getMessage());
-            return false;
+        if (!$result1) {
+            throw new \Exception("Failed to insert employee");
         }
-    } 
+
+        // Commit transaction
+        $this->db->commit();
+        return true;
+
+    } catch (\Exception $e) {
+        // Rollback transaction on error
+        $this->db->rollBack();
+        error_log("Job seeker registration transaction failed: " . $e->getMessage());
+        return false;
+    }
+}
 public function getJobSeekersByHierarchy(string $myBranchId, string $organizationId, int $limit = 50, int $offset = 0): array {
     $sql = "SELECT js.id, js.first_name, js.father_name, js.last_name,js.gender, b.name AS branch_name, b.level AS branch_level
             FROM job_seekers js
