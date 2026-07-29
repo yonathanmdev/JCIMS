4078a29d7917fc8ee843f8be7c73ce0fb840834b
tewodrosamare <teddyamare1621@gmail.com.com>
2026-07-29 17:21:14 +0300

    doneee

--- src/Models/JobCreationModel.php
+++ src/Models/JobCreationModel.php
@@ -10,74 +10,133 @@ class JobCreationModel {
     }
     // ይህን በModelህ ውስጥ ተጠቀምበት
  
-public function registerJobCreation($data) {
-    try {
-        // 1. Transaction እንጀምራለን
-        $this->db->beginTransaction();
-// 1. ቼክ ማድረግ: ይህ ስራ ፈላጊ በቋሚነት (Type 1) ቀድሞ ተቀጥሮ ይሆን?
-        $sqlCheck = "SELECT jobseeker_id FROM code003sraedl WHERE jobseeker_id = :js_id AND employment_type = 1";
-        $stmtCheck = $this->db->prepare($sqlCheck);
-        $stmtCheck->execute(['js_id' => $data['jobseeker_id']]);
-        
-        if ($stmtCheck->fetch()) {
-            // መረጃው ከተገኘ Transaction-ውን አቋርጠን Error እንወረውራለን
-            throw new Exception("ይህ ስራ ፈላጊ ቀድሞውኑ በቋሚነት ስራ ተፈጥሮለታል፤ ሌላ መዝገብ ማስገባት አይቻልም።");
-        }
-        // 2. በመጀመሪያ የ job_seekers ሰንጠረዥን እናዘምናለን (የቅጥር ሁኔታውን)
-        $sqlUpdate = "UPDATE job_seekers SET employment_status = :status WHERE job_seeker_id = :js_id";
-        $stmtUpdate = $this->db->prepare($sqlUpdate);
-        $stmtUpdate->execute([
-            'status' => $data['employment_type'],
-            'js_id'  => $data['jobseeker_id']
-        ]);
+public function getEnterpriseSectorAndSubsector($code003_id) {
+        // 1. በመጀመሪያ ከ code003 እና ከ junction_table የ enterprise_type እና አገናኝ IDዎችን እንወስዳለን
+        $sql = "SELECT 
+                    c.enterprise_type, 
+                    j.individual_enterprise_id, 
+                    j.team_id 
+                FROM code003 c
+                INNER JOIN junction_table_individual_and_team j 
+                    ON c.junction_table_id = j.table_id
+                WHERE c.code003_id = :code003_id 
+                LIMIT 1";
 
-        // 3. በመቀጠል የሥራ እድል መረጃን በ code003sraedl ውስጥ እናስገባለን
-        $sql = "INSERT INTO code003sraedl (
-                    uuid, branchid, code003_id, jobseeker_id, sector, subsector, 
-                    job_creation_reason, employment_type, employed_institution, 
-                    ssuportedname, fiscal_year, registered_by, job_field
-                ) VALUES (
-                    UNHEX(:uuid), :branchid, :code003_id, :jobseeker_id, :sector, :subsector, 
-                    :job_creation_reason, :employment_type, :employed_institution, 
-                    :suportedby, :fiscal_year, :registered_by, :job_field
-                )";
-        
         $stmt = $this->db->prepare($sql);
-        
-        // UUID v7 ማመንጨት
-        $data['uuid'] = $this->generateUuidV7();
-        
-        // SQL ላይ መጠየቂያዎቹ ከ $data ቁልፎች ጋር ተመሳሳይ እንዲሆኑ ማድረግ
-        // (SQL ውስጥ ':suportedby' ስንጠቀም በ $data ውስጥ 'suportedby' መኖሩን ያረጋግጣል)
-        $stmt->execute([
-            'uuid'                 => $data['uuid'],
-            'branchid'             => $data['branchid'],
-            'code003_id'           => $data['code003_id'],
-            'jobseeker_id'         => $data['jobseeker_id'],
-            'sector'               => $data['sector'],
-            'subsector'            => $data['subsector'],
-            'job_creation_reason'  => $data['job_creation_reason'],
-            'employment_type'      => $data['employment_type'],
-            'employed_institution' => $data['employed_institution'],
-            'suportedby'           => $data['suportedby'],
-            'fiscal_year'          => $data['fiscal_year'],
-            'registered_by'        => $data['registered_by'],
-            'job_field'            => $data['job_field'],
-        ]);
+        $stmt->execute(['code003_id' => $code003_id]);
+        $junctionData = $stmt->fetch(\PDO::FETCH_ASSOC);
 
-        // 4. ሁለቱም በተሳካ ሁኔታ ከተከናወኑ Commit እናደርጋለን
-        $this->db->commit();
-        return true;
-
-    } catch (\Exception $e) {
-        // ማንኛውም ስህተት ከተፈጠረ ሙሉ በሙሉ ወደ ኋላ ይመለሳል (Rollback)
-        if ($this->db->inTransaction()) {
-            $this->db->rollBack();
+        if (!$junctionData) {
+            return null;
         }
-        // ስህተቱን ወደ ሎግ እንዲጽፍ ወደ ኮንትሮለሩ እናስተላልፋለን
-        throw $e;
+
+        $subSectorId = null;
+
+        // 2. እንደ ኢንተርፕራይዙ አይነት (የግል ወይም ማህበር) sub_sector IDን እንወስዳለን
+        if ($junctionData['enterprise_type'] === 'የግል') {
+            $sqlInd = "SELECT sub_sector FROM individual_enterprise WHERE individual_ent_id = :ind_id LIMIT 1";
+            $stmtInd = $this->db->prepare($sqlInd);
+            $stmtInd->execute(['ind_id' => $junctionData['individual_enterprise_id']]);
+            $res = $stmtInd->fetch(\PDO::FETCH_ASSOC);
+            $subSectorId = $res['sub_sector'] ?? null;
+
+        } else if ($junctionData['enterprise_type'] === 'የማህበር') {
+            $sqlGroup = "SELECT sub_sector FROM group_table WHERE table_id = :team_id LIMIT 1";
+            $stmtGroup = $this->db->prepare($sqlGroup);
+            $stmtGroup->execute(['team_id' => $junctionData['team_id']]);
+            $res = $stmtGroup->fetch(\PDO::FETCH_ASSOC);
+            $subSectorId = $res['sub_sector'] ?? null;
+        }
+
+        if (!$subSectorId) {
+            return null;
+        }
+
+        // 3. የተገኘውን sub_sector ID በመጠቀም ከ sub_sector ጠረጴዛ ላይ sectorid እና sub_sectorid እንወስዳለን
+        $sqlSub = "SELECT sub_sectorid, sectorid FROM sub_sector WHERE sub_sectorid = :sub_id LIMIT 1";
+        $stmtSub = $this->db->prepare($sqlSub);
+        $stmtSub->execute(['sub_id' => $subSectorId]);
+        
+        return $stmtSub->fetch(\PDO::FETCH_ASSOC);
     }
-}
+
+    public function registerJobCreation($data) {
+        try {
+            // 1. Transaction እንጀምራለን
+            $this->db->beginTransaction();
+
+            // 2. ቼክ ማድረግ: ይህ ስራ ፈላጊ በቋሚነት (Type 1) ቀድሞ ተቀጥሮ ይሆን?
+            $sqlCheck = "SELECT jobseeker_id FROM code003sraedl WHERE jobseeker_id = :js_id AND employment_type = 1";
+            $stmtCheck = $this->db->prepare($sqlCheck);
+            $stmtCheck->execute(['js_id' => $data['jobseeker_id']]);
+            
+            if ($stmtCheck->fetch()) {
+                throw new Exception("ይህ ስራ ፈላጊ ቀድሞውኑ በቋሚነት ስራ ተፈጥሮለታል፤ ሌላ መዝገብ ማስገባት አይቻልም።");
+            }
+
+            // 3. ምክንያቱ 'አዳዲስ ኢንተርፕራይዞች በማቋቋም የተፈጠረ ሥራ' ከሆነ ዘርፍና ንዑስ ዘርፉን ከኢንተርፕራይዙ እንፈልጋለን
+            if ($data['job_creation_reason'] === "አዳዲስ ኢንተርፕራይዞች በማቋቋም የተፈጠረ ሥራ") {
+                $entInfo = $this->getEnterpriseSectorAndSubsector($data['code003_id']);
+                
+                if (!$entInfo) {
+                    throw new Exception("የተመረጠው ኢንተርፕራይዝ የዘርፍ እና ንዑስ ዘርፍ መረጃ አልተገኘለትም! እባክዎን የኢንተርፕራይዙን ምዝገባ ያረጋግጡ።");
+                }
+
+                $data['subsector'] = $entInfo['sub_sectorid'];
+                $data['sector']    = $entInfo['sectorid'];
+            }
+
+            // 4. የ job_seekers ሰንጠረዥን እናዘምናለን (የቅጥር ሁኔታውን)
+            $sqlUpdate = "UPDATE job_seekers SET employment_status = :status WHERE job_seeker_id = :js_id";
+            $stmtUpdate = $this->db->prepare($sqlUpdate);
+            $stmtUpdate->execute([
+                'status' => $data['employment_type'],
+                'js_id'  => $data['jobseeker_id']
+            ]);
+
+            // 5. የሥራ እድል መረጃን በ code003sraedl ውስጥ እናስገባለን
+            $sql = "INSERT INTO code003sraedl (
+                        uuid, branchid, code003_id, jobseeker_id, sector, subsector, 
+                        job_creation_reason, employment_type, employed_institution, 
+                        ssuportedname, fiscal_year, registered_by, job_field
+                    ) VALUES (
+                        UNHEX(:uuid), :branchid, :code003_id, :jobseeker_id, :sector, :subsector, 
+                        :job_creation_reason, :employment_type, :employed_institution, 
+                        :suportedby, :fiscal_year, :registered_by, :job_field
+                    )";
+            
+            $stmt = $this->db->prepare($sql);
+            
+            // UUID v7 ማመንጨት
+            $data['uuid'] = $this->generateUuidV7();
+            
+            $stmt->execute([
+                'uuid'                 => $data['uuid'],
+                'branchid'             => $data['branchid'],
+                'code003_id'           => $data['code003_id'],
+                'jobseeker_id'         => $data['jobseeker_id'],
+                'sector'               => $data['sector'],
+                'subsector'            => $data['subsector'],
+                'job_creation_reason'  => $data['job_creation_reason'],
+                'employment_type'      => $data['employment_type'],
+                'employed_institution' => $data['employed_institution'],
+                'suportedby'           => $data['suportedby'],
+                'fiscal_year'          => $data['fiscal_year'],
+                'registered_by'        => $data['registered_by'],
+                'job_field'            => $data['job_field'],
+            ]);
+
+            // 6. Commit እናደርጋለን
+            $this->db->commit();
+            return true;
+
+        } catch (\Exception $e) {
+            if ($this->db->inTransaction()) {
+                $this->db->rollBack();
+            }
+            throw $e;
+        }
+    }
 
     private function generateUuidV7() {
         $data = random_bytes(16);
