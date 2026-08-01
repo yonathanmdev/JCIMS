db01d4b30ca51316372b3eac3e12afd615f18c95
Yonathan Misgan Mengie <yonathanm.dev@gmail.com>
2026-08-01 16:56:37 +0300

    fayda trial2

--- src/Config/MenuConfig.php
+++ src/Config/MenuConfig.php
@@ -66,7 +66,7 @@ class MenuConfig
             [
                 'label' => 'ስራ ፈላጊ',
                 'icon'  => 'fas fa-users',
-                'roles' => ['team_leader', 'officer'],
+                'roles' => ['team_leader', 'officer', 'system_admin'],
                 'children' => [
                     [
                         'label' => 'ስራ ፈላጊ መመዝገብ',
@@ -82,8 +82,7 @@ class MenuConfig
                     [
                         'label' => 'እድሳት',
                         'url'   => '/jobseekers-renewal',
-                        'roles' => ['team_leader','officer'],
-                        'levels' => [3, 4]
+                        'roles' => ['system_admin'],
                     ],
                     [
                         'label' => 'እድሳት በፋይዳ',
